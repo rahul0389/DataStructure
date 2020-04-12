1f98fc8f751e497218baded42a5c11ab6254a5a2
Rahul Lamba <rahul.lamba@XE-GGN-IT-01265.XEBIAINDIA.LOCAL>
2020-04-12 20:14:08 +0400

    Updated project with recursion

---
+++ src/com/recursion/Factorial.java
@@ -0,0 +1,15 @@
+package com.recursion;
+
+public class Factorial {
+
+	public int factorial(int n)
+	{
+		if (n ==1){
+			return 1;
+		}
+		else
+		{
+			return n*factorial(n-1);
+		}
+	}
+}
---
+++ src/com/recursion/Recursion.java
@@ -0,0 +1,11 @@
+package com.recursion;
+
+public class Recursion {
+
+	public static void main(String args[])
+	{
+		Factorial fact = new Factorial();
+		int result = fact.factorial(5);
+		System.out.println(result);
+	}
+}
