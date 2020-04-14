d9ca2f983aac752af9973b55a48621485a4575fa
Rahul Lamba <rahul.lamba@XE-GGN-IT-01265.XEBIAINDIA.LOCAL>
2020-04-14 07:51:14 +0400

    Updated project with Node class

---
+++ src/com/node/SingleNode.java
@@ -0,0 +1,22 @@
+package com.node;
+
+public class SingleNode 
+{
+	private int value;
+	private SingleNode next;
+	
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public SingleNode getNext() {
+		return next;
+	}
+	public void setNext(SingleNode next) {
+		this.next = next;
+	}
+	
+
+}
