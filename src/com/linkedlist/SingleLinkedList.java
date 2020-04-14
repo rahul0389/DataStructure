dbe2c4bd8c8ad88c3f9606ab68ddd70b2ef84883
Rahul Lamba <rahul.lamba@XE-GGN-IT-01265.XEBIAINDIA.LOCAL>
2020-04-14 07:50:48 +0400

    Updated project with Singular Linked List

---
+++ src/com/linkedlist/SingleLinkedList.java
@@ -0,0 +1,18 @@
+package com.linkedlist;
+
+import com.node.SingleNode;
+
+public class SingleLinkedList 
+{
+
+	SingleNode head ;
+	SingleNode tail;
+	
+	//creation of singular linked list
+	public void createSingleLinkedList(int value)
+	{
+	  SingleNode node = new SingleNode();
+	  node.setValue(value);
+
+	}
+}
