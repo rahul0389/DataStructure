4bb2a6f1f9667fc73d7ffdd4897831293a410bb3
rahul0389 <rahul.lamba2489@gmail.com>
2020-04-21 23:28:29 +0400

    Create Double Linked list Main class

---
+++ src/com/linkedlist/DoubleLinkedListMain.java
@@ -0,0 +1,11 @@
+package com.linkedlist;
+
+public class DoubleLinkedListMain 
+{
+	public static void main(String args[])
+	{
+	DoubleLinkedList doublelist = new DoubleLinkedList();
+	doublelist.createDoubleLinkedList(5);
+	
+	}
+}
